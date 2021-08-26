c434480a58a11e280e1c758b48813f7890fbcbfe
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2021-08-25 21:12:19 -0500

    Add project files.

---
+++ OopWpfCommon/IWpfOopAddin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OopWpfCommon
+{
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("6A3968B5-FEBC-417F-8294-32C3D94B184A")]
+    public interface IWpfOopAddin
+    {
+        IntPtr Hwnd { get; }
+        void Shutdown();
+        bool HasFocusWithin();
+        bool TabInto(/* FocusNavigationDirection */ int direction, ref bool wrapped);
+
+        void ConnectToHost(IWpfOopAddinHost host);
+    }
+
+    [ComVisible(true)]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("6A3968B5-FEBC-417F-8294-32C3D94B184B")]
+    public interface IWpfOopAddinHost
+    {
+        bool OnNoMoreTabStops(/* FocusNavigationDirection */ int direction, ref bool wrapped);
+    }
+
+    public static class WpfOopAddinConstants
+    {
+        public const string ProgID = "OopWpfServer.WpfAddinHost";
+    }
+}
---
+++ OopWpfServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OopWpfServer
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            app.MainWindow = new MainWindow();
+            app.MainWindow.Show();
+            app.Run();
+        }
+    }
+}
