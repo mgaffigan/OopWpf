207add5f30b5a4417dee33ebf687a241fc897af3
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2023-11-14 23:15:06 -0600

    Missing from previous commit

--- Itp.WpfCrossProcess.Common/Properties/AssemblyInfo.cs
+++ Itp.WpfCrossProcess.Common/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 // Keep assembly version fixed to avoid breaking COM clients.
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: Guid("8a2080d6-53a7-4b6d-97d3-e635f35577f2")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Guid("C348704A-E901-4E7F-8B48-0DBDDE1AEAE5")]
